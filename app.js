6a6d6ffd9bd71daea5d5d3eabfa50f501f42d695
BurylovBFC57 <burylovbfc@gmail.com>
2024-09-13 09:17:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let hour = 24
+let minut = 60
+let sek = 60
+console.log(`1 задача: в сутках ${hour*minut*sek} секунд`)
+
+let hour2 = 24
+let minut2 = 60
+let sek2 = 60
+console.log(`2 задача: в 30 сутках ${hour2*30*minut2*sek2} секунд`)
+
+let hour3 = 24
+let minut3 = 60
+let sek3 = 60
+console.log(`3 задача: в году ${hour3*365*minut3*sek3} секунд`)
+
+let hour4 = 24
+let minut4 = 60
+console.log(`4 задача: в сутках ${hour4*minut4} минут`)
+
+let hour5 = 24
+let minut5 = 60
+console.log(`5 задача: в сутках ${hour5*365*minut5} минут`)
+
+let byte = 8
+let kilobyte = 
+
+console.log(`6 задача: в 30 сутках ${hour2*30*minut2*sek2} секунд`)
