e677cbb60e9c11ddf6e713892c8e0d4fff94c66d
BurylovBFC57 <burylovbfc@gmail.com>
2024-09-17 07:09:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,33 @@ let hour5 = 24
 let minut5 = 60
 console.log(`5 задача: в сутках ${hour5*365*minut5} минут`)
 
-let byte = 8
-let kilobyte = 
+let byte = 1024
+let Kb = 1024
 
-console.log(`6 задача: в 30 сутках ${hour2*30*minut2*sek2} секунд`)
+console.log(`6 задача: в 1 мегабайте ${byte*Kb} байт`)
+
+let b = 1024
+let Kib = 1024
+let Mb = 1024
+
+console.log(`7 задача: в 1 гигабайте ${b*Kib*Mb} байт`)
+
+let byt = 1024
+let Kilobyte = 1024
+let Megabyte = 1024
+
+console.log(`8 задача: в 10 гигабайтах ${byt*Kilobyte*Megabyte*10} байт`)
+
+let bt = 1024
+let Kibt = 1024
+let Mebt = 1024
+let Gb = 1024
+
+console.log(`9 задача: в 1 терабайте ${bt*Kibt*Mebt*Gb} байт`)
+
+let Kibyt = 1024
+let Megabt = 1024
+let Gigb = 1024
+
+console.log(`10 задача: в 1 терабайте ${Kibyt*Megabt*Gigb} килобайт`)
+
